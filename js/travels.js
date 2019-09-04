7c3b917e9199f6532dd3f42505b9d35ddb2b7b27
Lucie Straková <strakova.91@gmail.com>
2019-09-04 11:17:16 +0200

    added js to travels

---
+++ js/travels.js
@@ -0,0 +1,20 @@
+const containers = document.querySelectorAll('.image-content');
+const images = document.querySelectorAll('.image');
+
+containers.forEach(container => {
+   
+    const image = container.querySelector('.image');
+
+    container.addEventListener('mouseover', () => {
+        image.classList.add('opacity');    
+    });
+
+    container.addEventListener('mouseleave', () => {
+        image.classList.remove('opacity');
+    });
+
+});
+
+
+
+
