1e5a85a9f75e035b4f5dade31191524f313814be
Lucie Straková <strakova.91@gmail.com>
2019-09-05 00:10:13 +0200

    added database

---
+++ js/all_travels.js
@@ -0,0 +1,5 @@
+const headline = document.querySelector('.page-name-new');
+
+window.addEventListener('DOMContentLoaded', () => {
+    headline.classList.add('show-page-name');
+});
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-$(function() {
+// jquery scroll down chevron
 
-    // scroll down chevron
+$(function() {
 
     $(".fa-chevron-down").fadeIn(1000);
 
@@ -9,3 +9,27 @@ $(function() {
 
     });
 });
+
+// saving data to database
+
+const form = document.querySelector('form');
+const html = '<div class="response">Thank you, your email was added to our database.</div>';
+const container = document.querySelector('.email-sign');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const now = new Date();
+    const email = {
+        title: form.text.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    db.collection('emails').add(email).then(() => {
+        container.innerHTML += html;
+        const response = document.querySelector('.response');
+        setTimeout(() => response.innerText = '', 4000);
+    }).catch(err => {
+        console.log(err);
+    });
+    form.reset();
+});
+
