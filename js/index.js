9cdb471b9c25ce8e1bb2e70109f0042fb85b86ce
LucieSt <strakova.91@gmail.com>
2023-03-05 20:41:49 +0100

    js feature commented

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // jquery scroll down chevron
 
-$(function() {
+// $(function() {
 
-    $(".fa-chevron-down").fadeIn(1000);
+//     $(".fa-chevron-down").fadeIn(1000);
 
-    $(window).on('scroll', function() {
-        $(".fa-chevron-down").fadeOut(500);
+//     $(window).on('scroll', function() {
+//         $(".fa-chevron-down").fadeOut(500);
 
-    });
-});
+//     });
+// });
